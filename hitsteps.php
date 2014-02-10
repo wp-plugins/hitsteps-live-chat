5ea7c87eb2bcd3acdf2542bb0a59cdc4ef688900
hitsteps <hitsteps@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-10 18:57:17 +0000

    *update on floating chat widget
    git-svn-id: https://plugins.svn.wordpress.org/hitsteps-live-chat/trunk@854959 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hitsteps.php
+++ hitsteps.php
@@ -4,7 +4,7 @@ Plugin Name: Hitsteps Live Chat
 Plugin URI: http://www.hitsteps.com/
 Description: Hitsteps is a powerful visitor manager which allow you to monitor and engage with your website visitors and convert them into customers.
 Author: hitsteps.com
-Version: 1.04
+Version: 1.05
 Author URI: http://www.hitsteps.com/
 */ 
  
